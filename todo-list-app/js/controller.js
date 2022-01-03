f46acc45db29e6b0cce3afb56905453d09c4883d
KALASH2021 <dou.kolesnitchenko@gmail.com>
2022-01-03 10:40:03 +0100

    l160 modif

--- todo-list-app/js/controller.js
+++ todo-list-app/js/controller.js
@@ -157,7 +157,7 @@
 	 */
 	Controller.prototype.removeItem = function (id) {
 		var self = this;
-		var items;
+		/*var items;
 		self.model.read(function(data) {
 			items = data;
 		});
@@ -166,10 +166,11 @@
 			if (item.id === id) {
 				console.log("Element with ID: " + id + " has been removed.");
 			}
-		});
+		});*/
 
 		self.model.remove(id, function () {
 			self.view.render('removeItem', id);
+			console.log("Element with ID: " + id + " has been removed.");
 		});
 
 		self._filter();
